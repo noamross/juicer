4046b2a45b191192e7b59500b9060c7baa7b9a6e
Noam Ross <noam.ross@gmail.com>
2016-03-07 10:50:26 -0500

    Small wording changes based on win-builder results

--- tests/testthat/test-juice.R
+++ tests/testthat/test-juice.R
@@ -1,21 +1,21 @@
 context("juice")
 
-text_html = "<style>a {font-size:30;}</style><a href=\"http://www.ropensci.org\">ROpenSci</a>"
+text_html = "<style>a {font-size:30;}</style><a href=\"http://ropensci.org\">ROpenSci</a>"
 text_css = "a {font-size:30;}"
 
 test_that("juice works", {
-  expect_equal(juice(text_html), "<a href=\"http://www.ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>")
+  expect_equal(juice(text_html), "<a href=\"http://ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>")
   expect_equal(juice(text_html, css=text_css),
-               "<style>a {font-size:30;}</style><a href=\"http://www.ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>")
-  expect_equal(juice(text_html, options=list(applyStyleTags=FALSE)), "<a href=\"http://www.ropensci.org\">ROpenSci</a>")
+               "<style>a {font-size:30;}</style><a href=\"http://ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>")
+  expect_equal(juice(text_html, options=list(applyStyleTags=FALSE)), "<a href=\"http://ropensci.org\">ROpenSci</a>")
   expect_equal(juice(text_html, options=list(applyStyleTags=FALSE, removeStyleTags=FALSE)), text_html)
 })
 
 test_that("Multiple inputs", {
   expect_equal(juice(c(text_html, text_html)),
-               rep("<a href=\"http://www.ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>", 2))
+               rep("<a href=\"http://ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>", 2))
   expect_equal(juice(c(text_html, text_html), css=text_css),
-               rep("<style>a {font-size:30;}</style><a href=\"http://www.ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>", 2))
+               rep("<style>a {font-size:30;}</style><a href=\"http://ropensci.org\" style=\"font-size: 30;\">ROpenSci</a>", 2))
 })
 
 test_that("Expected failures", {
